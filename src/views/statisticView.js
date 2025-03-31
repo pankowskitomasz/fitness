1c4e7ec57fa81037a2462e64d73979f9e63a9f7a
pankowskitomasz <pankowski.email@gmail.com>
2025-03-31 10:33:24 +0200

    routing, layout: signin, signup, passwordupdate, restorepassword

---
+++ src/views/statisticView.js
@@ -0,0 +1,12 @@
+import {Container } from "react-bootstrap";
+
+function StatisticView(){
+    return(
+        <Container>
+            <h1>Statistic View</h1>
+
+        </Container>
+    );
+}
+
+export default StatisticView;
